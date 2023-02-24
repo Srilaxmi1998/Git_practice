e5f3368383463bc2c6e9537c22cbe5dfdcfccf69
Anumula <sranumula@teksystems.com>
2023-02-24 09:53:27 +0530

    complte

---
+++ java/src/java/Bike.java
@@ -0,0 +1,11 @@
+package java;
+
+public class Bike {
+
+	public static void main(String[] args) {
+		System.out.println("I am in bike main method");
+	}
+void speed() {
+	System.out.println("I am in bike speed method");
+}
+}
---
+++ java/src/java/MyFirstJava.java
@@ -0,0 +1,9 @@
+package java;
+
+public class MyFirstJava {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+	}
+
+}
